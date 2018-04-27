68450db05add29c8bff9e1be62aac6af262212a9
MilkywayHitchhiker <dark112365@naver.com>
2018-04-27 10:59:26 +0900

    ..

--- sysLog/sysLog/sysLog.cpp
+++ sysLog/sysLog/sysLog.cpp
@@ -8,17 +8,15 @@ hiker::CSystemLog *Log = hiker::CSystemLog::GetInstance (LOG_DEBUG);
 
 int main()
 {
-	int Cnt = 0;
-	int Cnt1 = 2;
-	int Cnt2 = 100;
-	Log->SetLogDirectory (L"../SYSTEM_LOG");
 
-	Log->Log (L"Network", LOG_DEBUG, L"LogLevel %d,%d,Cnt = %d", Cnt, Cnt1, Cnt2);
+	Log->SetLogDirectory (L"SYSTEM_LOG");
+
+	Log->Log (L"Network", LOG_SYSTEM, L"Network LogStart");
 	Sleep (1);
-	Log->Log (L"Game", LOG_DEBUG, L"LogLevel %d,%d,Cnt = %d", Cnt, 10, 1000);
+	Log->Log (L"Game", LOG_SYSTEM, L"Game LogStart");
 	Sleep (10);
 	Log->SetLogLevel (LOG_ERROR);
-	Log->Log (L"Network", LOG_DEBUG, L"LogLevel %d,%d,Cnt = %d", Cnt, 50, 10000);
+	Log->Log (L"Network", LOG_SYSTEM, L"Network LogEnd");
     return 0;
 }
 
